ede94f722a0a76056ebcb13ed512ba5b27e7dad5
Cydrick Nonog <cydrick.dev@gmail.com>
2017-04-10 04:59:24 +0800

    [FEATURE] Equation Functions was added. Exhance the codes to more readable Function added: roun

--- src/Number.php
+++ src/Number.php
@@ -11,6 +11,8 @@ class Number {
     protected $value;
     
     protected $original;
+    
+    private $localConfig;
 
     // Static Methods
     
@@ -30,6 +32,9 @@ class Number {
      * @return string
      */
     public static function format($num, $config = array()){
+        if ($num instanceof Number) {
+            $num = $num . '';
+        }
         if(!preg_match('/^([+-]?(\d+(\.\d*)?)|(\.\d+))$/', $num))
             throw new \Exception('Number is expecting 1 parameters to be a number.');
         
@@ -98,7 +103,7 @@ class Number {
      */
     public static function pow($num1, $exponent) {
         $num1 = new Number($num1);
-        return $num1->toPower($exponent);
+        return $num1->toPower($exponent . '');
     }
     
     /**
@@ -122,6 +127,7 @@ class Number {
      */
     public function __construct($num, $config = array()) {
         $config = array_replace_recursive(static::$config, $config);
+        $this->localConfig = $config;
         $num = $num.'';
         if(preg_match('/^([+-]?(\d+(\.\d*)?)|(\.\d+))$/', $num))
             $this->value = static::format ($num, $config);
@@ -182,6 +188,32 @@ class Number {
         $num = $this->format($num);
         $result = $this->value + $num;
         return new Number($result);
+    }
+    
+    public static function round() {
+        $args = func_get_args();
+        $static = !(isset($this) && get_class($this) == __CLASS__);
+        if($static) {
+            $args[0] = $args[0] .'';
+            if(!isset($args[1])) {
+                $args[1] = 0;
+            } else if($args[1] instanceof Number) {
+                $args[1] = $args[1] . '';
+            }
+            if(!isset($args[2])) {
+                $args[2] = PHP_ROUND_HALF_UP;
+            } else if($args[2] instanceof Number) {
+                $args[2] = $args[2] . '';
+            }
+            $num = round($args[0], $args[1], $args[2]);
+            return new Number($num);
+        } else {
+            $num = $this . '';
+            if(!isset($args[0])) $args[0] = 0;
+            if(!isset($args[1])) $args[1] = PHP_ROUND_HALF_UP;
+            $num = round($num, $args[0], $args[1]);
+            return new Number($num);
+        }
     }
     
     /**
@@ -301,99 +333,183 @@ class Number {
      * @return \Cydrickn\Number\Number
      */
     public static function parseEquation($string, $vars = array(), $compute = true) {
-        $index = 0;
-        $array_parse = array();
-        $type = null;
-        for($i = 0;$i < strlen($string);$i++) {
-            $char = $string[$i];
-            if($char == '(') {
-                
-                if($type == 'num' || $type == 'arr') {
-                    $index++;
-                    $array_parse[$index] = '*';
-                    $index++;
-                }
-                
-                $type = 'arr';
-                $parse = "";
-                $open = 0;
-                for($ii = $i+1; $ii < strlen($string);$ii++) {
-                    if($string[$ii] == "(") $open++;
-                    if($string[$ii] == ")") $open--;
-                    
-                    if($open == -1) break;
-                    else $parse .= $string[$ii];
-                }
-                $i = $ii;
-                if(array_key_exists($index, $array_parse)) {
-                    $index++;
-                }
-                if($compute)
-                    $array_parse[$index] = self::parseEquation($parse, $vars, $compute);
-                else $array_parse[$index] = '('.self::parseEquation($parse, $vars, $compute).')';
-            } else if(self::isOperator($char) && $type != 'opr' && !is_null($type)) {
-                if(array_key_exists($index, $array_parse)) {
-                    $index++;
-                }
-                $type = 'opr';
-                $array_parse[$index] = $char;
-            } else {
-                if(array_key_exists($index, $array_parse) && $type != 'num') {
-                    if($type == 'arr') {
-                        $index++;
-                        $array_parse[$index] = '*';
-                    }
-                    $index++;
-                    $array_parse[$index] = '';
-                } else if(!array_key_exists($index, $array_parse)) {
-                    $array_parse[$index] = '';
-                }
-                $type = 'num';
-                $array_parse[$index] .= $char;
+        $functions = self::getFunctionsEq();
+        $funcs = array();
+        $string = str_replace(' ', '', $string);
+        $string = trim(preg_replace('/([0-9]+)/', '#$1#', $string));
+        foreach($functions as $func) {
+            $count = 0;
+            $string = str_replace($func, '$' . (count($funcs)), $string, $count);
+            if($count > 0) {
+                $funcs[] = $func;
             }
         }
-        self::subtitute($array_parse, $vars);
-        if($compute)
-            return (new Number(self::compute($array_parse, $vars)));
-        return implode('',$array_parse);
+        
+        $string = self::normalize($string, $vars);
+        if(!$compute) {
+            foreach($funcs as $key => $func) {
+                $string = str_replace('$' . $key, $func, $string);
+            }
+            $string = str_replace('#', '', $string);
+            return $string;
+        }
+        
+        return new Number(self::parseEq($string, $funcs)['value']);
     }
     
-    private static function subtitute(&$array, $vars = array()) {
-        foreach($array as &$arr) {
-            if(!self::isOperator($arr)) {
-                if(is_string($arr) && array_key_exists($arr, $vars)) {
-                    $arr = $vars[$arr];
+    private static function parseEq($str, $funcs) {
+        $index = 0;
+        $tokens = array();
+        $isNew = true;
+        $open = 0;
+        for ($i = 0; $i < strlen($str); $i++) {
+            $char = $str[$i];
+            if($char == "$") {
+                $tokens[$index] = array('type' => 'FUNC', 'func' => null,'value' => '');
+                
+                for($x = $i+1; $x < strlen($str); $x++) {
+                    if($str[$x] != '(') {
+                        $tokens[$index]['func'] = $str[$x];
+                        continue;
+                    }
+
+                    $tokens[$index]['func'] = $funcs[$tokens[$index]['func']];
+                    break;
                 }
+                $i = $x+1;
+                
+                $tokens[$index]['value'] = self::findEnd($str, $i, '(', ')');
+            } else if ($char == '(') {
+                $i++;
+                $tokens[$index] = array('type' => 'PAR','value' => '');
+                $tokens[$index]['value'] = self::findEnd($str, $i, '(', ')');
+            } else if(self::isOperator($char)) {
+                $tokens[$index] = array('type' => 'OPR','value' => $char);
+            } else if($char == '#'){
+                $i++;
+                $tokens[$index] = array('type' => 'NUM','value' => '');
+                $tokens[$index]['value'] = self::findEnd($str, $i, null, '#');
+            }
+            
+            if($tokens[$index]['type'] == 'PAR') {
+                $tokens[$index] = self::parseEq($tokens[$index]['value'], $funcs);
+            } else if($tokens[$index]['type'] == 'FUNC') {
+                $tokens[$index]['value'] = self::parseFunc($tokens[$index]['value'], $funcs);
+                $tokens[$index]['value'] = self::executeFunction($tokens[$index]['func'], $tokens[$index]['value']) . '';
+            }
+            
+            $index++;
+        }
+        
+        return self::compute($tokens);
+    }
+    
+    private static function findEnd($str, &$index, $open, $close) {
+        $string = '';
+        $count = 1;
+        for($i = $index; $i < strlen($str); $i++) {
+            $char =  $str[$i];
+            if($char == $open) {
+                if($count > 0) {
+                    $string .= $char;
+                }
+                $count++;
+            } else if($char == $close) {
+                $count--;
+                if($count > 0) {
+                    $string .= $char;
+                } else {
+                    break;
+                }
+            } else {
+                $string .= $char;
             }
         }
+        $index = $i;
+        return $string;
+    }
+    
+    private static function parseFunc($parse, $funcs) {
+        $sps = array();
+        $_open = 0;
+        $_parse = '';
+        $_index = 0;
+        
+        for($x = 0; $x < strlen($parse); $x++) {
+            $c = $parse[$x];
+            if($c == '(') {
+                $_open++;
+
+            } else if($c == ')') {
+                $_open--;
+            }
+
+            if($_open == 0 && $c == ',') {
+                $_index++;
+            } else {
+                if(!isset($sps[$_index])) {
+                    $sps[$_index] = '';
+                }
+                $sps[$_index] .= $c;
+            }
+        }
+
+        $args = array();
+        foreach($sps as $sp) {
+            $args[] = self::parseEq($sp, $funcs)['value'];
+        }
+        
+        return $args;
+    }
+    
+    public static function normalize($string, $vars = array()) {
+        
+        $string = trim(preg_replace('/((?<=[0-9A-Za-z])(?=[A-Za-z])|(?<=[A-Za-z])(?=[0-9A-Za-z]))/', "*", $string), '*');
+        
+        $string = str_replace(')$', ')*$', $string);
+        $string = str_replace(')(', ')*(', $string);
+        
+        foreach($vars as $key => $var) {
+            $val = $var;
+            if(is_numeric($val)) {
+                $val = "#$val#";
+            }
+            $string = str_replace($key, $val, $string);
+        }
+        
+        return $string;
     }
     
     private static function compute($array) {
         $new_array = $array;
         foreach(self::getOperators() as $opr) {
-            for($index = 0;$index < count($new_array);$index++) {
-                $val = $new_array[$index];
-                if(self::isOperator($val) && $val === $opr) {
-                    $num1 = $new_array[$index-1];
-                    $num2 = $new_array[$index+1];
-                    $num = new Number($num1);
-                    switch ($val) {
-                        case '+': 
-                            $num = $num->plus($num2);break;
-                        case '-': 
-                            $num = $num->minus($num2);break;
-                        case '*': 
-                            $num = $num->times($num2);break;
-                        case '/': 
-                            $num = $num->dividedBy($num2);break;
-                        case '%': 
-                            $num = $num->modulo($num2);break;
-                        case '^':
-                            $num = $num->toPower($num2);break;
-                        default: $num = new Number($num2);
+            if(count($new_array) > 1) {
+                for($index = 0;$index < count($new_array);$index++) {
+                    $val = $new_array[$index]['value'];
+                    $type = $new_array[$index]['type'];
+                    if($type == 'OPR' && $val === $opr) {
+                        $num1 = $new_array[$index-1]['value'];
+                        $num2 = $new_array[$index+1]['value'];
+                        $num = new Number($num1);
+                        switch ($val) {
+                            case '+': 
+                                $num = $num->plus($num2);break;
+                            case '-': 
+                                $num = $num->minus($num2);break;
+                            case '*': 
+                                $num = $num->times($num2);break;
+                            case '/': 
+                                $num = $num->dividedBy($num2);break;
+                            case '%': 
+                                $num = $num->modulo($num2);break;
+                            case '^':
+                                $num = $num->toPower($num2);break;
+                            default: $num = new Number($num2);
+                        }
+                        $num = array(array('type' => 'NUM', 'value' => $num . ''));
+                        array_splice($new_array, $index-1, 3, $num);
+                        $index-=1;
                     }
-                    array_splice($new_array, $index-1, 3, $num . '');
-                    $index-=1;
                 }
             }
         }
@@ -415,6 +531,31 @@ class Number {
         }
         return $has;
     }
+    
+    private static function isFunctionEq($func) {
+        $fns = self::getFunctionsEq();
+        
+        return in_array($func, $fns);
+    }
+    
+    private static function getFunctionsEq() {
+        return array(
+            'add',
+            'sub',
+            'mul',
+            'div',
+            'round',
+            'pow'
+        );
+    }
+    
+    private static function executeFunction($func, $arguments) {
+        $ars = array();
+        foreach ($arguments as $arg) {
+            $ars[] = $arg . '';
+        }
+        return call_user_func_array(get_class() .'::' . $func, $arguments);
+    } 
     
     
     // End of mehods
